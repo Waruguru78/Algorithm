81ec3cc6047b9929cce1ca9845bf3a295b0ec001
Wangui <lizkim685@gmail.com>
2024-08-05 11:32:32 +0300

    An algorithm that reads a sentence character by character.

--- count.js
+++ count.js
@@ -3,8 +3,8 @@ function insertionSort(arr) {
         let current = arr[i];
         let j = i - 1;
 
-        // Move elements of arr[0..i-1], that are greater than current,
-        // to one position ahead of their current position
+        /* Move elements of arr[0..i-1], that are greater than current,
+        to one position ahead of their current position */
         while (j >= 0 && arr[j] > current) {
             arr[j + 1] = arr[j];
             j--;
